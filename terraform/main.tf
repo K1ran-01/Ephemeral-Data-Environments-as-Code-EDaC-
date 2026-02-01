d50239c880e6b75603685e3279c9708c44ec907d
H B Kiran Gowda <kirangowda6651@gmail.com>
2026-02-01 15:05:31 +0530

    Base-setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,21 +38,3 @@ resource "databricks_grants" "sp_usage" {
     privileges = ["ALL_PRIVILEGES"]
   }
 }
-
-# Output the name of the created catalog for use in other steps/scripts
-output "catalog_name" {
-  value       = databricks_catalog.pr_sandbox.name
-  description = "The name of the ephemeral Unity Catalog created for the PR."
-}
-
-# Output the ID of the created SQL warehouse for use in other steps/scripts
-output "warehouse_id" {
-  value       = databricks_sql_warehouse.pr_warehouse.id
-  description = "The ID of the ephemeral SQL Warehouse created for the PR."
-}
-
-# Output the HTTP path for the created SQL warehouse
-output "warehouse_http_path" {
-  value       = "/sql/1.0/warehouses/${databricks_sql_warehouse.pr_warehouse.id}"
-  description = "The HTTP path for the ephemeral SQL Warehouse."
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,15 @@ output "catalog_name" {
 output "warehouse_id" {
   value = databricks_sql_warehouse.pr_warehouse.id
 }
+
+# Output the name of the created catalog for use in other steps/scripts
+output "catalog_name" {
+  value       = databricks_catalog.pr_sandbox.name
+  description = "The name of the ephemeral Unity Catalog created for the PR."
+}
+
+# Output the HTTP path for the created SQL warehouse
+output "warehouse_http_path" {
+  value       = "/sql/1.0/warehouses/${databricks_sql_warehouse.pr_warehouse.id}"
+  description = "The HTTP path for the ephemeral SQL Warehouse."
+}
